b6d235d82a71eaf1073af5afbb17de2fd6bb9b37
Alex Charles <alexscharles@gmail.com>
2014-01-17 18:02:32 -0600

    Added constructor to build the array

--- BattleBlock/web/tetris.dart
+++ BattleBlock/web/tetris.dart
@@ -5,13 +5,25 @@ class Tetris {
 	double x, y;
 	CanvasRenderingContext2D ctx;
 
-	List<List<int>> board;
+	int numRows = 22;
+	int numCols = 10;
+	List<List<int>> board = new List(22);
+
+	Tetris() {
+		for(int i=0; i<numRows; i++) {
+			board[i] = new List(10);
+		}
+	}
 
 	void Update(dt) {
 
 	}
 
 	void Draw() {
+
+	}
+
+	void AddToBottom(num howMany) {
 
 	}
 }
