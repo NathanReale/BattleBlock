327315dfbf77124b1b8fbd878dfa453cc55ae442
Nathan Reale <nathan.reale@gmail.com>
2014-01-17 18:28:19 -0600

    Added a keyboard class to track key presses

--- BattleBlock/web/keyboard.dart
+++ BattleBlock/web/keyboard.dart
@@ -1,3 +1,25 @@
-class Keyboard {
+library keyboard;
 
+import 'dart:html';
+
+class Keyboard {
+  Map<int, int> _keys = new Map<int, int>();
+
+  Keyboard() {
+    window.onKeyDown.listen((KeyboardEvent e) {
+      // If the key is not set yet, set it with a timestamp.
+      if (!_keys.containsKey(e.keyCode))
+        _keys[e.keyCode] = e.timeStamp;
+    });
+
+    window.onKeyUp.listen((KeyboardEvent e) {
+      _keys.remove(e.keyCode);
+    });
+  }
+
+	isPressed(int keyCode, [bool clear = false]) {
+  		var ret = _keys.containsKey(keyCode);
+		if (clear) _keys.remove(keyCode);
+		return ret;
+  }
 }
