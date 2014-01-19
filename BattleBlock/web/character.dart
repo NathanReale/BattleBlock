5aede8126313d7b3ac3719b88a0b4589b5272130
Nathan Reale <nathan.reale@gmail.com>
2014-01-19 11:20:13 -0600

    Added magic nuke

--- BattleBlock/web/character.dart
+++ BattleBlock/web/character.dart
@@ -40,12 +40,22 @@ void magic_invert(Tetris cur, Tetris opp) {
 	}
 }
 
+void magic_nuke(Tetris cur, Tetris opp) {
+	for (int r = 0; r < Tetris.numRows; r++) {
+		for (int c = 0; c < Tetris.numCols; c++) {
+			if (opp.board[r][c] == 1 && rand.nextDouble() > 0.5) {
+				opp.board[r][c] = 0;
+			}
+		}
+	}
+}
+
 
 class Character {
 	final Function m1, m2, m3, m4;
 	const Character(this.m1, this.m2, this.m3, this.m4);
 
-	static const TED = const Character(magic_slide, magic_drop, magic_queue, magic_drop);
+	static const TED = const Character(magic_slide, magic_drop, magic_queue, magic_nuke);
 	static const BILL = const Character(magic_drop, magic_slide, magic_steal, magic_invert);
 }
 
