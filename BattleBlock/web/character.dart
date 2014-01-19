19c3ad9908d4724de4d1aa7bab0ea7aafcc2b087
Alex Charles <alexscharles@gmail.com>
2014-01-19 11:14:39 -0600

    Switched Bill and Ted, made block spawn higher

--- BattleBlock/web/character.dart
+++ BattleBlock/web/character.dart
@@ -27,6 +27,7 @@ void magic_queue(Tetris cur, Tetris opp) {
 	cur.magic = opp.magic;
 	opp.magic = 0;
 }
+
 void magic_invert(Tetris cur, Tetris opp) {
 
 	for(int i=opp.findTopRow(); i<Tetris.numRows; i++) {
@@ -45,7 +46,7 @@ class Character {
 	final Function m1, m2, m3, m4;
 	const Character(this.m1, this.m2, this.m3, this.m4);
 
-	static const TED = const Character(magic_slide, magic_drop, magic_queue, magic_drop);
 	static const BILL = const Character(magic_drop, magic_slide, magic_steal, magic_invert);
+	static const TED = const Character(magic_slide, magic_drop, magic_queue, magic_drop);
 }
 
